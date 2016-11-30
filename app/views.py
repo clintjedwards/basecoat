699c48df67b89e325838c5b1cb611bb60681f44c
Clint Edwards <clint.j.edwards@gmail.com>
2016-11-30 18:01:01 -0500

    Adding logo and making database autogenerate

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, jsonify
+from flask import render_template
 import basecoat.db_utils as db
 
 from app import app
@@ -6,12 +6,7 @@ from app import app
 
 @app.route('/')
 def index():
-    return render_template('index.html')
-
-
-@app.route('/api/formulas')
-def get_formulas():
     formula_table = db.get_table('Formula')
     formula_list = [formula_table.to_dict() for formula_table in formula_table]
-
-    return jsonify(formula_list)
+    return render_template('index.html',
+                           formula_list=formula_list)
--- tools/database.py
+++ tools/database.py
@@ -5,11 +5,34 @@ from app import db, models
 def populate_db():
     "Populates database with fake data. Useful for developement"
 
-    dev_formula = models.Formula(color_name='flaky magenta',
-                                 color_number='FM-4563',
-                                 customer_name='Edwards Inc.',
-                                 summary='A Flaky summer color with a tinge of orange',
-                                 notes='The Red 52 is broken use the Red 51')
+    dev_formula1 = models.Formula(color_name='Soft Red',
+                                  color_number='SR-EC644B',
+                                  customer_name='Edwards Inc.',
+                                  summary='Etiam iaculis nulla ac ex euismod tempor. Sed suscipit lorem id urna porttitor ullamcorper.',
+                                  notes='In egestas magna eu turpis condimentum venenatis.')
+    dev_formula2 = models.Formula(color_name='Ripe Lemon',
+                                  color_number='RL-F7CA18',
+                                  customer_name='Edwards Inc.',
+                                  summary='Curabitur facilisis diam a pharetra scelerisque. In quam mi, dapibus at ligula et, blandit ultricies dolor.',
+                                  notes='Aliquam eget urna euismod, consectetur tellus et, auctor metus.')
+    dev_formula3 = models.Formula(color_name='Light Wisteria',
+                                  color_number='LW-BE90D4',
+                                  customer_name='Edwards Inc.',
+                                  summary='',
+                                  notes='')
+    dev_formula4 = models.Formula(color_name='Testcolorlongname',
+                                  color_number='TC-BE90D4-BE90D4-BE90D4',
+                                  customer_name='Edwards Inc.',
+                                  summary='Curabitur facilisis diam a pharetra scelerisque. In quam mi, dapibus at ligula et, blandit ultricies dolor.' +
+                                          'Curabitur facilisis diam a pharetra scelerisque. In quam mi, dapibus at ligula et, blandit ultricies dolor.' +
+                                          'Curabitur facilisis diam a pharetra scelerisque. In quam mi, dapibus at ligula et, blandit ultricies dolor.',
+                                  notes='')
+
+    dev_formula5 = models.Formula(color_name='Tcs',
+                                  color_number='TC',
+                                  customer_name='Edwards Inc.',
+                                  summary='',
+                                  notes='')
 
     dev_colorant = models.Colorant(formula_id=1,
                                    colorant_name='sunset orange',
@@ -20,7 +43,7 @@ def populate_db():
                            product_name='benjamin moore & co')
 
     try:
-        db.session.add_all([dev_formula, dev_colorant, dev_base])
+        db.session.add_all([dev_formula1, dev_formula2, dev_formula3, dev_formula4, dev_formula5, dev_colorant, dev_base])
         db.session.commit()
         print 'Populated database'
     except:

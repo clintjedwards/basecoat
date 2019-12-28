bb87ec1dd9058ef71908c2e5594b088f6e11132e
Clint J Edwards <clint.j.edwards@gmail.com>
2019-12-28 14:37:57 -0500

    Adding 404 page
    Fixes #35

---
+++ frontend/src/components/NotFound.vue
@@ -0,0 +1,15 @@
+<template>
+  <h2 class="not-found-text">(404) Page Not Found</h2>
+</template>
+
+<script lang="ts">
+export default {};
+</script>
+
+<style scoped>
+.not-found-text {
+  padding-top: 20px;
+  margin-left: auto;
+  margin-right: auto;
+}
+</style>
--- frontend/src/router.ts
+++ frontend/src/router.ts
@@ -1,63 +1,63 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import CreateFormulaModal from "./components/CreateFormulaModal.vue";
+import CreateJobModal from "./components/CreateJobModal.vue";
+import FormulasPage from "./components/FormulasPage.vue";
+import JobsPage from "./components/JobsPage.vue";
+import LoginModal from "./components/LoginModal.vue";
+import ManageFormulaModal from "./components/ManageFormulaModal.vue";
+import ManageJobModal from "./components/ManageJobModal.vue";
+import NotFound from "./components/NotFound.vue";
 
-import LoginModal from "./components/LoginModal.vue"
-import FormulasPage from "./components/FormulasPage.vue"
-import CreateFormulaModal from "./components/CreateFormulaModal.vue"
-import ManageFormulaModal from "./components/ManageFormulaModal.vue"
-import JobsPage from "./components/JobsPage.vue"
-import CreateJobModal from "./components/CreateJobModal.vue"
-import ManageJobModal from "./components/ManageJobModal.vue"
-
-
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
-    { path: '/', redirect: '/formulas' },
-    {
-        path: '/formulas',
-        name: 'formulas',
-        component: FormulasPage,
-        children: [
-            {
-                path: 'create',
-                name: 'createFormulaModal',
-                component: CreateFormulaModal
-            },
-            {
-                path: ':id',
-                name: 'manageFormulaModal',
-                component: ManageFormulaModal
-            }
-        ]
-    },
-    {
-        path: '/jobs',
-        name: 'jobs',
-        component: JobsPage,
-        children: [
-            {
-                path: 'create',
-                name: 'jobCreateModal',
-                component: CreateJobModal
-            },
-            {
-                path: ':id',
-                name: 'manageJobModal',
-                component: ManageJobModal
-            }
-        ]
-    },
-    {
-        path: '/login',
-        name: 'login',
-        component: LoginModal,
-    },
-]
+  { path: "/", redirect: "/formulas" },
+  {
+    path: "/formulas",
+    name: "formulas",
+    component: FormulasPage,
+    children: [
+      {
+        path: "create",
+        name: "createFormulaModal",
+        component: CreateFormulaModal
+      },
+      {
+        path: ":id",
+        name: "manageFormulaModal",
+        component: ManageFormulaModal
+      }
+    ]
+  },
+  {
+    path: "/jobs",
+    name: "jobs",
+    component: JobsPage,
+    children: [
+      {
+        path: "create",
+        name: "jobCreateModal",
+        component: CreateJobModal
+      },
+      {
+        path: ":id",
+        name: "manageJobModal",
+        component: ManageJobModal
+      }
+    ]
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: LoginModal
+  },
+  { path: "*", component: NotFound }
+];
 
 const router = new VueRouter({
-    routes,
-    mode: 'history'
-})
+  routes,
+  mode: "history"
+});
 
-export default router
+export default router;
